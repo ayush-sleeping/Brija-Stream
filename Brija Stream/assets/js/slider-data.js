83546958b241ab9c4e80ae46acd7c86ce39f7b1d
Ayush Mishra <ayushbm84@gmail.com>
2023-09-03 00:14:48 +0530

    Added required files

---
+++ Brija Stream/assets/js/slider-data.js
@@ -0,0 +1,51 @@
+let movies = [
+  {
+    name: "House of Dragon", // Movie name
+    moreInfo: "PG | 18 •  Action, Adventure • 2022 | 1 seasons", // Additional information about the movie
+    des: "An internal succession war within House Targaryen at the height of its power, 172 years before the birth of Daenerys Targaryen...!", // Movie description (placeholder text)
+    image: "https://static.hbo.com/2022-06/house-of-the-dragon-ka-1920.jpg", // Path to the movie's image
+    watch: "▶ Watch Now", // Option to watch the movie
+  }, 
+  {
+    name: "Joker",
+    moreInfo: "PG | 18 • Crime, Drama, Thriller • 2019 | 122 min",
+    des: "The rise of Arthur Fleck, from aspiring stand-up comedian and pariah to Gotham's clown prince and leader of the revolution...!",
+    image: "https://m.media-amazon.com/images/M/MV5BMGQ1ZGZmNTAtM2MyYi00NmZhLTkwYmYtNTNlZDRhMzU2ZTgwXkEyXkFqcGdeQW1yb3NzZXI@._V1_.jpg",
+    watch: "▶ Watch Now",
+  },
+  {
+    name: "The Mandalorian",
+    moreInfo: "PG | 18 • Sci-Fi, Action, Advanture • 2019 | 3 seasons",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit.!",
+    image: "https://techcrunch.com/wp-content/uploads/2023/01/jzD5HPJMFP83mnn90BT59f9YJ21.jpg",
+    watch: "▶ Watch Now",
+  },
+  {
+    name: "Venom",
+    moreInfo: "PG | 18 • Action, Adventure, Sci-Fi • 2018 | 1 hr 52 min",
+    des: "A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. But the being takes a liking to Earth and decides to protect it...!",
+    image: "https://m.media-amazon.com/images/M/MV5BOWVhZTE4MTAtNDdkYS00NTRjLTk1ZWUtMTBmZjk4N2M0NmJiXkEyXkFqcGdeQW1yb3NzZXI@._V1_QL75_UX500_CR0,3,500,281_.jpg",
+    watch: "▶ Watch Now",
+  },
+  {
+    name: "Deadpool",
+    moreInfo: "PG | 18 • Action, Comedy • 2019 | 1h 48m",
+    des: "A wisecracking mercenary gets experimented on and becomes immortal yet hideously scarred, and sets out to track down the man who ruined his looks...!",
+    image: "https://img.mensxp.com/media/content/2022/Jun/Thor-Might-Make-A-Cameo-In-Deadpool-31400_629de8576f405.jpeg",
+    watch: "▶ Watch Now",
+  },
+  {
+    name: "SEE",
+    moreInfo: "PG | 18 • Action, Drama, Sci-Fi • 2018 | 3 seasons",
+    des: "Far in a dystopian future, the human race has lost the sense of sight, and society has had to find new ways to interact, build, hunt, and to survive...!",
+    image: "https://is5-ssl.mzstatic.com/image/thumb/Features112/v4/3a/f2/23/3af2237c-b647-db35-e023-52463bf49ae7/e96143e1-5c88-46b7-8a49-20d7bc60b4d1.lsr/1478x832fe.webp",
+    watch: "▶ Watch Now",
+  },
+  {
+    name: "DARK",
+    moreInfo: "PG | 18 • Crime, Drama, Mystery • 2017 | 3 seasons",
+    des: "A family saga with a supernatural twist, set in a German town where the disappearance of two young children exposes the relationships among four families...!",
+    image: "https://dark.jakel.rocks/share/global.png",
+    watch: "▶ Watch Now",
+  },
+];
