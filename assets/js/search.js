32d73ebbb42e186a7a85c9b5e70221bb9e9004f2
Ayush Mishra <ayushbm84@gmail.com>
2023-09-03 01:00:49 +0530

    updated http to https

--- assets/js/search.js
+++ assets/js/search.js
@@ -67,7 +67,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=6b7e239f`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=6b7e239f`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
             displayMovieDetails(movieDetails);
